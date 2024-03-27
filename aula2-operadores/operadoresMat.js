04e705c8d8b9d6f661fc4362dd7c609de07faec4
alexandrerussi <alexandrerussijunior@gmail.com>
2024-03-26 22:23:52 -0300

    aula de javascript inicial

---
+++ aula2-operadores/operadoresMat.js
@@ -0,0 +1,40 @@
+let numero1 = 6;
+let numero2 = 2;
+
+console.log(" ");
+console.log("----------------------------");
+console.log(`Número 1: ${numero1}`);
+console.log(`Número 2: ${numero2}`);
+console.log("----------------------------");
+console.log(" ");
+
+// OPERADORES
+
+// SOMA
+
+let soma = numero1 + numero2; // operação matemática
+console.log("A soma é: " + soma); // concatenação
+console.log(`A soma é: ${soma}`); // template string
+
+// SUBTRAÇÃO
+
+let subtracao = numero1 - numero2;
+console.log("Sub: " + subtracao);
+
+// DIVISÃO
+// numero2 = 0;
+let divisao = numero1 / numero2;
+console.log("Divisão: " + divisao);
+
+// MULTI
+let multi = numero1 * numero2;
+console.log("Multi: "+ multi);
+
+// POTENCIA
+let potencia = numero1 ** numero2;
+console.log("Potência: " + potencia)
+
+// MÓDULO -- RESTO
+numero1 = 5;
+let resto = numero1 % numero2;
+console.log("Resto da divisão: " + resto);
